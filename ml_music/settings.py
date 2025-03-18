052cdc50979bd811de251ed0fdf8947e4c49cf96
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2025-03-18 13:54:07 +0300

    adding tests 2.0

--- ml_music/settings.py
+++ ml_music/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-from . import secrets
+import ml_music.secrets as secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
